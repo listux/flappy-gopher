c7d157512b301e2fea68a326b48af1f1c03a0676
listux <livnat.stav@gmail.com>
2019-06-05 10:23:29 +0300

    updates bird, scene; adds pipes

--- main.go
+++ main.go
@@ -36,7 +36,7 @@ func run() error {
 
 	sdl.PumpEvents()
 
-	if err := drawTitle(r); err != nil {
+	if err := drawTitle(r, "Flappy Gopher"); err != nil {
 		return fmt.Errorf("could not draw title: %v", err)
 	}
 
@@ -61,7 +61,7 @@ func run() error {
 	}
 }
 
-func drawTitle(r *sdl.Renderer) error {
+func drawTitle(r *sdl.Renderer, text string) error {
 	r.Clear()
 
 	f, err := ttf.OpenFont("res/fonts/Flappy.ttf", 20)
@@ -71,7 +71,7 @@ func drawTitle(r *sdl.Renderer) error {
 	defer f.Close()
 
 	c := sdl.Color{R: 255, G: 100, B: 0, A: 255}
-	s, err := f.RenderUTF8Solid("Flappy Gopher", c)
+	s, err := f.RenderUTF8Solid(text, c)
 	if err != nil {
 		return fmt.Errorf("could not render title: %v", err)
 	}
